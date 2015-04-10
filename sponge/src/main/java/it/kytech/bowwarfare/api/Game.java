efe16bed3cf0096ed2a48c61f794f056663ac447
Nicolò Veronese <nicveronese@gmail.com>
2015-04-10 20:42:50 +0200

    Added Api Classes

---
+++ sponge/src/main/java/it/kytech/bowwarfare/api/Game.java
@@ -0,0 +1,7 @@
+package it.kytech.bowwarfare.api;
+
+/**
+ * Created by M2K on 10/04/2015.
+ */
+public class Game {
+}
---
+++ sponge/src/main/java/it/kytech/bowwarfare/api/events/PlayerDead.java
@@ -0,0 +1,7 @@
+package it.kytech.bowwarfare.api.events;
+
+/**
+ * Created by M2K on 10/04/2015.
+ */
+public class PlayerDead {
+}
